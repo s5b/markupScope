e948aae77d679ec1e1f5f87e77bcbab2775cf998
S t u a r t <github@sequation.com.au>
2013-05-05 14:24:55 +1000

    Added more tests.

--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -10,11 +10,27 @@ Capybara.app = Sinatra::Application
 Capybara.javascript_driver = :poltergeist
 Capybara.always_include_port=true
 
-feature 'Listing Name' do
+feature 'Listing' do
 
-  it 'should include the name of the listing e heading' do
-    visit '/directory-listing/acme'
-    find('#name').should have_content('ACME')
+  [
+      {kind: 'business', name: 'acme'},
+      {kind: 'government', name: 'senate'},
+      {kind: 'residential', name: 'smith'},
+      {kind: '', name: 'doNotCare'}
+  ].map do |test_case|
+
+    it 'should include the name of the listing as a heading' do
+      visit "/directory-listing/#{test_case[:name]}"
+
+      find('h1#name').should have_content(test_case[:name].upcase)
+    end
+
+    it 'should include the type of the listing in the content' do
+      visit "/directory-listing/#{test_case[:name]}"
+
+      page.should have_content("This is a #{test_case[:kind]} listing.")
+    end
+
   end
 
 end
